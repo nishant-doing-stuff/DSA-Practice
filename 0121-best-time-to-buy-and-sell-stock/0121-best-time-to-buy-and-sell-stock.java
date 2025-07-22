c78b2f1e05bdf7a9754acf7c5db03c6f5df25898
Nishant Jain <nishant.upskilling@gmail.com>
2025-07-22 08:09:31 +0530

    Time: 3 ms (5.86%), Space: 62 MB (29.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,14 +2,21 @@ class Solution {
     // pattern : prefix sun 
     // space : O(N)
     // time : O(N)
+    
+    // pattern2 : kadane's algorithm
+    // space : O(1)
+    // time : O(N)
+    // a1 , a2 , a3 , a4 , a5 
+    // lets assume a5>a3 , and this gives the max profix 
+    // so ans is a5 - a3
+    // we can just then take the sum of differenced between a2 and a5 ( difference is a[i] - a[i-1]) ( price change between today and yester day )
+    // = a5 - a4 + a4 - a3 + a3 - a2 = a5 - a2 = RHS 
+    // this this problem can be solved like this aswell 
     public int maxProfit(int[] prices) {
-        int [] prefix = new int [prices.length + 1];
-        for(int i = prices.length-1 ; i>=0 ; i--){
-            prefix[i] = Math.max(prices[i],prefix[i+1]);
-        }
-        int maxVal = 0;
-        for( int i = 0;i<prices.length ; i++ ){
-            maxVal = Math.max(maxVal , prefix[i]-prices[i]);
+        int maxVal = 0,maxCur = 0;
+        for( int i = 1 ; i<prices.length ; i++){
+             maxCur = Math.max(0,maxCur + prices[i]-prices[i-1]);
+            maxVal = Math.max(maxVal,maxCur);
         }
         return maxVal;
     }   
