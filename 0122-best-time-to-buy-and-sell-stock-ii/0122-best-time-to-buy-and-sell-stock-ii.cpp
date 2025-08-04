277374705df134df1be939ed78f1a918bced53ac
Nishant Jain <nishant.upskilling@gmail.com>
2025-08-04 21:18:45 +0530

    Time: 11 ms (6.61%), Space: 25.5 MB (5.29%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return solution(0, 0, dp, prices);
+    }
+    int solution(int curIdx, int didBought, vector<vector<int>>& dp,
+                 vector<int>& prices) {
+        if (curIdx == prices.size())
+            return 0;
+        if (dp[curIdx][didBought] != -1)
+            return dp[curIdx][didBought];
+        int profit = INT_MIN;
+        if (didBought) {
+            // sell
+            int sell = prices[curIdx] + solution(curIdx + 1, 0, dp, prices);
+            // dontSell
+            int dontSell = solution(curIdx + 1, didBought, dp, prices);
+            return dp[curIdx][didBought] = max(sell, dontSell);
+        } else {
+            // buy
+            int buy = solution(curIdx, 1, dp, prices) - prices[curIdx];
+            // dontBuy
+            int didnBuy = solution(curIdx + 1, didBought, dp, prices);
+            return dp[curIdx][didBought] = max(buy, didnBuy);
+        }
+    }
+
+    // use memioisation
+    // int maxProfit(vector<int>& prices) { return recursion(0, -1, prices);
+
+    // }
+
+    // // rather than keeping the index of bought at i can just keep a boolean
+    // as
+    // // to keep track of wheteher i hav bought or not ;
+    // int recursion(int curIdx, int boughtAtIdx, vector<int>& prices) {
+    //     if (curIdx == prices.size())
+    //         return 0;
+    //     // if bought
+    //     int profit = 0;
+    //     if (boughtAtIdx >= 0) {
+    //         // sell now
+    //         int sellNowProfit = (prices[curIdx] - prices[boughtAtIdx]) +
+    //                             recursion(curIdx + 1, -1, prices);
+    //         // don't sell now
+    //         int notSellProfit = recursion(curIdx + 1, boughtAtIdx, prices);
+    //         profit = max({0, sellNowProfit, notSellProfit});
+    //     }
+    //     // if not bought
+    //     else {
+    //         // buy now
+    //         int buyNowProfit = recursion(curIdx, curIdx, prices);
+    //         // don't buy now
+    //         int dontBuyNowProfit = recursion(curIdx + 1, boughtAtIdx,
+    //         prices); profit = max({0, buyNowProfit, dontBuyNowProfit});
+    //     }
+    //     return profit;
+    // }
+};
