27ae846e07074d915ec195b34444f163731f2a20
Nishant Jain <nishant.upskilling@gmail.com>
2025-08-04 08:25:33 +0530

    Time: 1 ms (39.3%), Space: 11.2 MB (31.4%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
+    vector<int> countBits(int n) {
+    vector<int> ans(max(n + 1, 2), 0);
+    ans[0] = 0;
+    ans[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        int curMod = 1 << (int)floor(log2(i));
+        // cout << i << " " << curMod << endl;
+        int curIndex = i%curMod;
+        int curVal = 1 + ans[curIndex];
+        ans[i] = curVal;
+    }
+    return vector<int>(ans.begin(), ans.begin() + n + 1);
+}
+
     // brute force solution 
     // nLogN
-    vector<int> countBits(int n) {
+    vector<int> countBits_brute(int n) {
         vector<int> ans(n+1,0);
         for(int i = 0;i<=n;i++)
             ans[i] = getCount(i);
