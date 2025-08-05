4b9e00250ff8486a95f3dca36c9030735db7f3af
Nishant Jain <nishant.upskilling@gmail.com>
2025-08-05 18:07:22 +0530

    Time: 0 ms (100%), Space: 71.8 MB (16.69%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // kadane's algorithm
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0] ; 
+        int curSum = nums[0] ;
+        for(int i = 1 ; i<nums.size();i++){
+             curSum = max(nums[i],curSum + nums[i]);
+            maxSum= max(maxSum,curSum);
+        }
+        return maxSum;
+    }
+};
