1dc29356fb81cd56ea2e41f76d3775978af41a89
Nishant Jain <nishant.upskilling@gmail.com>
2025-08-06 10:26:39 +0530

    Time: 2 ms (34.35%), Space: 19.5 MB (5.1%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -4,11 +4,14 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int countFresh=0;
         queue<pair<int,int>> q1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2)
                     q1.push({i, j});
+                if(grid[i][j] == 1) 
+                    countFresh++;
             }
         }
         vector<pair<int,int>> directions = {
@@ -31,18 +34,21 @@ public:
                     if (newX >= 0 && newY >= 0 && newX < n && newY < m && grid[newX][newY] == 1) {
                         grid[newX][newY] = 2;
                         q1.push({newX, newY});
+                        countFresh--;
                     }
                 }
             }
         }
-
+        // we can remove this by just coutning the number of fresh and if the left fresh == 0 then we can retun the ans ';
+        if(countFresh != 0)
+            return -1;
         // if still some oranges left then return -1; 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1)
-                    return -1;
-            }
-        }
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         if (grid[i][j] == 1)
+        //             return -1;
+        //     }
+        // }
         return time == 0 ? 0 : time - 1; // fix off-by-one
     }
 };
